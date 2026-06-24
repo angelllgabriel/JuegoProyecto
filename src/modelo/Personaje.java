94b51561b8cbf9ced4073836880eb836bb38fb7b
Angel <angelllgabriel90@gmail.com>
2026-06-23 22:09:51 -0600

    Personaje completo

--- src/modelo/Personaje.java
+++ src/modelo/Personaje.java
@@ -2,4 +2,21 @@ public class Personaje{
     private String nombre;
     private int vida;
 
+    public Personaje(String nombre, int vida){
+        this.nombre = nombre;
+        this.vida = vida;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getVida(){
+        return vida;
+    }
+
+    public void mostrarInformacion(){
+        System.out.println("Nombre del personaje: "+ nombre);
+        System.out.println("Vida del personaje: "+ vida);
+    }
 }
